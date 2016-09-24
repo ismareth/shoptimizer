37b112b487599e716706b1f00b2773d3eb6d95c8
Ramit Suri <ramitsuri007@gmail.com>
2016-09-24 14:50:55 -0400

    added routes and app.js

---
+++ node/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var mongoose = require('mongoose');
+
+// var shoppingListRoute = require('./routes/shoppingListRoute.js');
+// var mapRoute = require('./routes/mapRoute.js');
+// var billingRoute = require('./routes/billingRoute.js');
+var storesRoute = require('./routes/storesRoute.js');
+
+var app = express();
+
+var ipAddress = "localhost:27017";
+var dbName = "shoptimizer";
+var port = 1399;
+
+//mongoose.connect('mongodb://'+ ipAddress +'/'+ dbName +'');
+// app.use('/shoppinglist', shoppingListRoute);
+// app.use('/map', mapRoute);
+// app.use('/billing', billingRoute);
+app.use('/store', storesRoute);
+
+app.listen(port);
+console.log("Starting on port " + port);
