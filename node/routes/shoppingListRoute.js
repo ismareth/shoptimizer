454f2fcc919e6a025cb8ba9bc1bf9029c34d9002
Malvika Singh <malvika0901@gmail.com>
2016-09-24 22:54:03 -0400

    Getting Aisle Numbers from API-1

--- node/routes/shoppingListRoute.js
+++ node/routes/shoppingListRoute.js
@@ -14,23 +14,33 @@ router.post('/', function(req, res, next) {
     
 for(var i = 0; i < shoppingList.length; i++) {
     var obj = shoppingList[i];
-    var aisleNumber=[];
+    var aisleNum=[];
     var cheapestAisle=[];
-    int k=0;
+    var lookup={};
     var url='http://www.supermarketapi.com/api.asmx/COMMERCIAL_SearchForItem?APIKEY='+apiKey+'&StoreId='+storeID+'&ItemName='+obj;
+        
         request(url, function (error, response, body) {
+        	   
         	  var parseString = require('xml2js').parseString;
+
               parseString(response.body, function (err, result) {
+
                  var productArray=result.ArrayOfProduct_Commercial.Product_Commercial;
                  for(var j=0;j<productArray.length;j++)
                  {  
-                 	console.log(productArray[j].AisleNumber);
-
+                 	var aisle=productArray[j].AisleNumber;
+                 	if (!(aisle in lookup)) {
+                     lookup[aisle] = 1;
+                       aisleNum.push(aisle);
+                          }
+                        
                  }
-             });
-
+              });
+               
+             console.log(aisleNum);
+             aisleNum=[];
           });
-
+           
  }
 
    
